44569f3ad3a31a55fcb4865321933f583024adac
safwan-ms <safwan.anasms@gmail.com>
2025-09-21 21:02:33 +0530

    Id Validation using DTO

---
+++ 05. DTO Validation/01. Id Validation/users.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  ParseIntPipe,
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+
+type RoleEnum = "INTERN" | "ENGINEER" | "ADMIN";
+
+type UserType = {
+  id: number;
+  name: string;
+  email: string;
+  role: RoleEnum;
+};
+
+type UpdateUser = Omit<UserType, "id">;
+
+@Controller("users")
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get(":id")
+  findOne(@Param("id", ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(":id")
+  update(
+    @Param("id", ParseIntPipe) id: number,
+    @Body() userUpdate: UpdateUser
+  ) {
+    return this.usersService.update(id, userUpdate);
+  }
+
+  @Delete(":id")
+  delete(@Param("id", ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
+  }
+}
